87dd116780128032836c03cddb8cb1dc574f90bf
huahua <moflowerlkh@gmail.com>
2023-03-13 15:59:28 +0800

    before merge

--- src/main/java/org/kaihua/obliop/collection/FbsVector.java
+++ src/main/java/org/kaihua/obliop/collection/FbsVector.java
@@ -1,14 +1,21 @@
 package org.kaihua.obliop.collection;
 
-import com.google.flatbuffers.FlatBufferBuilder;
-import org.kaihua.obliop.collection.fbs.*;
-import org.kaihua.obliop.data.ObliData;
-import sun.nio.ch.DirectBuffer;
-
-import java.io.Serializable;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.kaihua.obliop.collection.fbs.DoubleValue;
+import org.kaihua.obliop.collection.fbs.Field;
+import org.kaihua.obliop.collection.fbs.FieldUnion;
+import org.kaihua.obliop.collection.fbs.IntValue;
+import org.kaihua.obliop.collection.fbs.Row;
+import org.kaihua.obliop.collection.fbs.RowTable;
+import org.kaihua.obliop.collection.fbs.StringValue;
+import org.kaihua.obliop.data.ObliData;
+
+import com.google.flatbuffers.FlatBufferBuilder;
+
+import sun.nio.ch.DirectBuffer;
 
 /**
  * @author kahua.li
@@ -98,7 +105,7 @@ public class FbsVector {
     offset = RowTable.createRowTable(builder, offset);
     builder.finish(offset);
     ByteBuffer bytbuf = builder.dataBuffer();
-    builder.clear();
+    // builder.clear();
     return bytbuf;
   }
 
