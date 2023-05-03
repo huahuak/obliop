10fc0d11512eab9a98cbd508380a2e750f6c488f
huahua <moflowerlkh@gmail.com>
2023-05-03 16:04:03 +0800

    adjust block size

--- src/main/java/org/kaihua/obliop/config.java
+++ src/main/java/org/kaihua/obliop/config.java
@@ -1,6 +1,6 @@
 package org.kaihua.obliop;
 
 public class Config {
-  public static int blockNumber = (int) Math.pow(10, 5);
-  public static int blockSize = 20 * 1024 * 1024;
+  public static int blockNumber = (int) Math.pow(10, 3);
+  public static int blockSize = 100 * 1024;
 }
