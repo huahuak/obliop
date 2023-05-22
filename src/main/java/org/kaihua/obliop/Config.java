e7b07e421a5906ac8bfcb15473052fd853308513
huahua <moflowerlkh@gmail.com>
2023-05-22 13:44:41 +0800

    config

--- src/main/java/org/kaihua/obliop/Config.java
+++ src/main/java/org/kaihua/obliop/Config.java
@@ -1,6 +1,6 @@
 package org.kaihua.obliop;
 
 public class Config {
-  public static int blockNumber = (int) Math.pow(10, 4);
-  public static int blockSize = 1024 * 1024;
+  public static int blockNumber = (int) Math.pow(10, 3);
+  public static int blockSize = 100 * 1024;
 }
--- src/main/java/org/kaihua/obliop/config.java
+++
@@ -1,6 +0,0 @@
-package org.kaihua.obliop;
-
-public class Config {
-  public static int blockNumber = (int) Math.pow(10, 3);
-  public static int blockSize = 100 * 1024;
-}
